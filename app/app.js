ba671e6d01fc514772f5335daf8d7e7aae6c9db6
John Keisker <jkeisker@me.com>
2016-04-04 14:49:30 -0500

    changes

---
+++ app/app.js
@@ -0,0 +1,31 @@
+let todos = [];
+
+
+
+
+document.querySelector("#toDoSubmit")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+    let infoField = document.querySelector("#toDoInput");
+    console.log(infoField.value);
+    let todo = new Todo(infoField.value);
+    todos.push(todo);
+    render();
+  })
+
+
+function Todo(item) {
+  this.item = item;
+  this.completed = false;
+}
+
+function render() {
+  let list = document.querySelector("#toDo");
+  list.innerHTML = "";
+  todos.forEach((todo) => {
+    let li = document.createElement("li");
+    li.textContent = todo.item;
+    list.appendChild(li);
+  })
+}
+render();
