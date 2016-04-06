5ad0a65fa994090414faa5399bf3ca3a0cb4fdac
John Keisker <jkeisker@me.com>
2016-04-06 14:30:55 -0500

    styling changes

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ document.querySelector("#toDoSubmit")
     console.log(infoField.value);
     let todo = new Todo(infoField.value);
     todos.push(todo);
+    infoField.value = "";
     render();
   })
 
@@ -18,11 +19,47 @@ function Todo(item) {
 
 function render() {
   let list = document.querySelector("#toDo");
-  list.innerHTML = "";
-  todos.forEach((todo) => {
-    let li = document.createElement("li");
-    li.textContent = todo.item;
-    list.appendChild(li);
+
+  // document.querySelector
+  let oldTodos = document.querySelectorAll("#toDo li");
+
+  Array.from(oldTodos).forEach((todo) => {
+    todo.removeEventListener("click", (e) => {
+      markComplete(todo);
+    })
   })
+
+  // loop through existing todos, call removeEventListener
+  // on each todo
+
+  list.innerHTML = "";
+
+
+  todos.forEach((todo) => {
+      let li = document.createElement("li");
+      li.textContent = todo.item;
+      list.appendChild(li);
+
+      // add event listener to the li
+      li.addEventListener('click', (e) => {
+        markComplete(todo);
+      });
+
+      // add a class if the todo is completed
+      if (todo.completed) {
+        li.classList.add("completed");
+      }
+    })
+
 }
+
+function markComplete(todo) {
+  if (todo.completed) {
+    todo.completed = false;
+  } else {
+    todo.completed = true;
+  }
+  render();
+}
+
 render();
